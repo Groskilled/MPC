b626f212bf3c35920d52e9d54fca657824122b2a
Adam Wybierala <adam.wybierala@gmail.com>
2018-02-05 16:06:48 +0100

    version working on the VM

--- src/main.cpp
+++ src/main.cpp
@@ -91,6 +91,13 @@ int main() {
 			double py = j[1]["y"];
 			double psi = j[1]["psi"];
 			double v = j[1]["speed"];
+			double delta = j[1]["steering_angle"];
+			double accel = j[1]["throttle"];
+			const double cos_psi = cos(-psi);
+			const double sin_psi = sin(-psi);
+
+			double throttle_value;
+			double steer_value;
 
 			/*
 			 * TODO: Calculate steering angle and throttle using MPC.
@@ -98,77 +105,70 @@ int main() {
 			 * Both are in between [-1, 1].
 			 *
 			 */
+			px = px + v * cos(psi) * 0.1;
+			py = py + v * sin(psi) * 0.1;
+			psi = psi + v * delta / 2.67 * 0.1;
+			v = v + accel * 0.1;
 
-			double ptsx_copy;
-			for (int i = 0; i < ptsx.size(); i++)
+			Eigen::VectorXd car_pt_x(ptsx.size());
+			Eigen::VectorXd car_pt_y(ptsy.size());
+
+			for (size_t i = 0; i < ptsx.size(); i++)
 			{
-				ptsx_copy = ptsx[i];
-				ptsx[i] = (ptsx[i] - px) * cos(-psi) - (ptsy[i] - py) * sin(-psi);
-				ptsy[i] = (ptsx_copy - px) * sin(-psi) - (ptsy[i] - py) * cos(psi);
+				double x = ptsx[i] - px;
+				double y = ptsy[i] - py;
+				car_pt_x[i] = x * cos_psi - y * sin_psi;
+				car_pt_y[i] = x * sin_psi + y * cos_psi;
 			}
 
-			double* ptrx = &ptsx[0];
-			Eigen::Map<Eigen::VectorXd> ptsx_transform(ptrx, 6);
 
-			double* ptry = &ptsy[0];
-			Eigen::Map<Eigen::VectorXd> ptsy_transform(ptry, 6);
-
-			auto coeffs = polyfit(ptsx_transform, ptsy_transform, 3);
-
-			double cte = polyeval(coeffs, 0);
+			auto coeffs = polyfit(car_pt_x, car_pt_y, 3);
+			double cte = coeffs[0];
 			double epsi = -atan(coeffs[1]);
-
-			double steer_value = j[1]["steering_angle"];
-			double throttle_value = j[1]["throttle"];
 
 			Eigen::VectorXd state(6);
 			state << 0, 0, 0, v, cte, epsi;
 
 			auto vars = mpc.Solve(state, coeffs);
 
-			vector<double> next_x_vals;
-			vector<double> next_y_vals;
+			steer_value = vars[0] / deg2rad(25);
+			throttle_value = vars[1];
 
-			double poly_inc = 2.5;
-			int num_points = 25;
-			for (int i = 0; i < num_points; i++)
-			{
-				next_x_vals.push_back(poly_inc * i);
-				next_y_vals.push_back(polyeval(coeffs, poly_inc * i));
-			}
+			std::cout << "delta = " << steer_value << std::endl;
+			std::cout << "a = " << throttle_value << std::endl;
+			std::cout << std::endl;
+
+			json msgJson;
+			msgJson["steering_angle"] = -steer_value;
+			msgJson["throttle"] = throttle_value;
 
 			vector<double> mpc_x_vals;
 			vector<double> mpc_y_vals;
 
-			for (int i = 2; i < vars.size(); i+=2)
+			for (size_t i = 2; i < vars.size(); i+=2)
 			{
 				mpc_x_vals.push_back(vars[i]);
 				mpc_y_vals.push_back(vars[i + 1]);
 			}
 
-
-			json msgJson;
-			// NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
-			// Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-			msgJson["steering_angle"] = vars[0] / deg2rad(25);
-			msgJson["throttle"] = vars[1];
-
-			//Display the MPC predicted trajectory 
-
-			//.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
-			// the points in the simulator are connected by a Green line
-
 			msgJson["mpc_x"] = mpc_x_vals;
 			msgJson["mpc_y"] = mpc_y_vals;
 
 			//Display the waypoints/reference line
+			vector<double> next_x_vals;
+			vector<double> next_y_vals;
+
+			for (size_t i = 1; i <= vars.size(); ++i)
+			{
+				next_x_vals.push_back(i);
+				next_y_vals.push_back(polyeval(coeffs, i));
+			}
 
 			//.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
 			// the points in the simulator are connected by a Yellow line
 
 			msgJson["next_x"] = next_x_vals;
 			msgJson["next_y"] = next_y_vals;
-
 
 			auto msg = "42[\"steer\"," + msgJson.dump() + "]";
 			std::cout << msg << std::endl;
@@ -192,36 +192,38 @@ int main() {
 			}
 	});
 
-	// We don't need this since we're not using HTTP but if it's removed the
-	// program
-	// doesn't compile :-(
-	h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
-				size_t, size_t) {
-			const std::string s = "<h1>Hello world!</h1>";
-			if (req.getUrl().valueLength == 1) {
-			res->end(s.data(), s.length());
-			} else {
-			// i guess this should be done more gracefully?
-			res->end(nullptr, 0);
-			}
-			});
+  // We don't need this since we're not using HTTP but if it's removed the
+  // program
+  // doesn't compile :-(
+  h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
+                     size_t, size_t) {
+    const std::string s = "<h1>Hello world!</h1>";
+    if (req.getUrl().valueLength == 1) {
+      res->end(s.data(), s.length());
+    } else {
+      // i guess this should be done more gracefully?
+      res->end(nullptr, 0);
+    }
+  });
 
-	h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
-			std::cout << "Connected!!!" << std::endl;
-			});
+  h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
+    std::cout << "Connected!!!" << std::endl;
+  });
 
-	h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
-				char *message, size_t length) {
-			ws.close();
-			std::cout << "Disconnected" << std::endl;
-			});
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
+                         char *message, size_t length) {
+    ws.close();
+    std::cout << "Disconnected" << std::endl;
+  });
 
-	int port = 4567;
-	if (h.listen(port)) {
-		std::cout << "Listening to port " << port << std::endl;
-	} else {
-		std::cerr << "Failed to listen to port" << std::endl;
-		return -1;
-	}
-	h.run();
+  int port = 4567;
+  if (h.listen(port)) {
+    std::cout << "Listening to port " << port << std::endl;
+  } else {
+    std::cerr << "Failed to listen to port" << std::endl;
+    return -1;
+  }
+  h.run();
+
+
 }
